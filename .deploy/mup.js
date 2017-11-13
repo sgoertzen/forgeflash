625eb380358ab3acb9e7d2268598f9da17559324
Shawn Goertzen <sgoertzen@gmail.com>
2017-11-13 13:26:28 -0800

    Deploy to Digital Ocean

---
+++ .deploy/mup.js
@@ -0,0 +1,58 @@
+module.exports = {
+  servers: {
+    one: {
+      // TODO: set host address, username, and authentication method
+      host: '104.131.73.205',
+      username: 'root',
+      // pem: './path/to/pem'
+      // password: 'server-password'
+      // or neither for authenticate from ssh-agent
+    }
+  },
+
+  app: {
+    // TODO: change app name and path
+    name: 'forgeflash',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      // TODO: Change to your app's url
+      // If you are using ssl, it needs to start with https://
+      ROOT_URL: 'http://forge.allmote.com',
+      MONGO_URL: 'mongodb://localhost/meteor',
+    },
+
+    // ssl: { // (optional)
+    //   // Enables let's encrypt (optional)
+    //   autogenerate: {
+    //     email: 'email.address@domain.com',
+    //     // comma separated list of domains
+    //     domains: 'website.com,www.website.com'
+    //   }
+    // },
+
+    docker: {
+      // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
+      image: 'abernix/meteord:node-8.4.0-base',
+    },
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
+  }
+};
